30fbb8f19347ea9cd59217dd151c1f2d4e04a106
Oresti <hoxhallarioresti02@gmail.com>
2024-04-16 17:15:19 +0200

    Update the js on main

--- script.js
+++ script.js
@@ -13,3 +13,45 @@
 // title.addEventListener("mouseleave", function () {
 //   title.style.color = "white";
 // });
+
+
+
+
+//ORESTI MAIN JS
+const SecCard = document.getElementById("section-epinal");
+
+const createCard = (resto) => {
+  const div = document.createElement('div');
+  div.classList.add("cards")
+
+  const template = `<div class="cards">
+        <div class="card">
+          <div class="card-header">
+            <div class="card-img" style="background-image: url('${resto.background}')"></div>
+            <div class="card-body">
+              <h2 class="nameResto">${resto.name}</h2>
+              <h3 id="stars">${"&#11088;".repeat(resto.stars)}</h3>
+              <img class="sushiicon"  src="./img/${resto.src}" alt="${resto.name}">
+            </div>
+          </div>
+        </div>
+      </div>
+  `;
+  div.innerHTML = template;
+  
+  return div ;
+
+};
+
+for (let i = 0; i < restos.length; i++) {
+  SecCard.appendChild(createCard(restos[i]));
+}
+
+
+// Première etape, injecter les 3 blocs de villes vides....
+
+
+// Récupérer les 3 bloc distincts
+
+// UNe fois les 3 blocs de villes injectés (mis dans le DOM)
+// Faire la boucle sur les restos avec des if else pour injecter dans le bon bloc de ville
