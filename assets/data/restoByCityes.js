689d0a19f4b90c5868de495e64f4494a9309fe9c
Oresti <hoxhallarioresti02@gmail.com>
2024-04-18 16:05:33 +0200

    The updated main js

--- assets/data/restoByCityes.js
+++ assets/data/restoByCityes.js
@@ -25,5 +25,88 @@ const restos = [
     stars: 4,
     category: "Veggie",
     city: "Epinal"
-  }
+  },
+  {
+    id: 3,
+    name: "Dao Sushi",
+    src: "sushi.png",
+    background: "https://images.unsplash.com/photo-1611143669185-af224c5e3252?q=80&w=2664&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 4,
+    category: "Sushi",
+    city: "Epinal"
+  },
+
+  {
+    id: 4,
+    name: "Au soleil Levant",
+    src: "sushi.png",
+    background: "https://images.unsplash.com/photo-1651326852735-7ba1e0f8364e?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 5,
+    category: "Sushi",
+    city: "Nantes"
+  },
+  {
+    id: 5,
+    name: "Istanbul Kebap",
+    src: "burger.png",
+    background: "https://images.unsplash.com/photo-1529006557810-274b9b2fc783?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 4,
+    category: "Burger",
+    city: "Nantes"
+  },
+  {
+    id: 6,
+    name: "L'Éthiquête",
+    src: "veggie.png",
+    background: "https://images.unsplash.com/photo-1555243896-c709bfa0b564?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 5,
+    category: "Veggie",
+    city: "Nantes"
+  },
+  {
+    id: 7,
+    name: "Roadside",
+    src: "burger.png",
+    background: "https://images.unsplash.com/photo-1613319300832-a105da5bd34e?q=80&w=2767&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 5,
+    category: "Burger",
+    city: "Nantes"
+  },
+  {
+    id: 8,
+    name: "WASABI",
+    src: "sushi.png",
+    background: "https://static.wixstatic.com/media/e18ebd_46b5c686ae2a4676a32d271ea441fe0e~mv2.jpeg/v1/fill/w_535,h_535,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/e18ebd_46b5c686ae2a4676a32d271ea441fe0e~mv2.jpegv",
+    stars: 4,
+    category: "Sushi",
+    city: "Lyon"
+  },
+  {
+    id: 9,
+    name: "YAAFA",
+    src: "burger.png",
+    background: "https://www.heteroclite.org/wp-content/uploads/2018/10/shoot_01_yaafa_01_grosplan1.jpg",
+    stars: 4,
+    category: "Burger",
+    city: "Lyon"
+  },
+  {
+    id: 10,
+    name: "Culina Hortus",
+    src: "veggie.png",
+    background: "https://images.happycow.net/venues/1024/13/85/hcmp138581_607449.jpeg",
+    stars: 4,
+    category: "Veggie",
+    city: "Lyon"
+  },
+  {
+    id: 11,
+    name: "Prana restaurant",
+    src: "veggie.png",
+    background: "https://images.unsplash.com/photo-1603046891741-c030dee9df65?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    stars: 5,
+    category: "Veggie",
+    city: "Lyon"
+  },
+  
 ];
